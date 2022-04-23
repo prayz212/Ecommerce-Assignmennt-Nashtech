40948b044f1ff6b9b59969ae98442c8d74ec2358
prayz212 <49938194+prayz212@users.noreply.github.com>
2022-04-23 21:39:06 +0700

    Update UrlRequest.cs

--- CustomerSite/Utils/UrlRequest.cs
+++ CustomerSite/Utils/UrlRequest.cs
@@ -5,7 +5,7 @@ namespace CustomerSite.Utils
         public static string GET_URL_PRODUCTS_BY_CATEGORY(string category, int page, int size) => $"products/?category={category}&page={page}&size={size}";
         public static string GET_URL_PRODUCT_BY_ID(int id) => $"products/{id}";
         public static string GET_URL_FEATURE_PRODUCTS(int page, int size) => $"products/features?page={page}&size={size}";
-        public static string GET_URL_RELATIVE_PRODUCTS(int id, int size) => $"products/{id}?size={size}";
+        public static string GET_URL_RELATIVE_PRODUCTS(int id, int size) => $"products/relative/{id}?size={size}";
         public static string GET_URL_RATING_PRODUCT() => "products/rating";
         public static string GET_URL_CATEGORIES() => "categories";
     }
